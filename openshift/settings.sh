7cd51c085a9cb9e03b9de2cd4c9970d349c92d66
Wade Barnes <wade.barnes@shaw.ca>
2017-12-13 12:35:59 -0800

    Remove Solr

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -14,7 +14,7 @@ export PROD=prod
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export -a skip_git_overrides=("schema-spy-build.json" "solr-base-build.json")
+export -a skip_git_overrides=("schema-spy-build.json")
 export GIT_URI="https://github.com/bcgov/Family-Protection-Order.git"
 export GIT_REF="master"
 
@@ -40,16 +40,16 @@ export GLUSTER_SVC_CONFIG=https://raw.githubusercontent.com/BCDevOps/openshift-t
 export GLUSTER_SVC_NAME=glusterfs-cluster-app
 
 # The project components
-export -a components=("tob-db" "tob-solr" "tob-api" "tob-web")
+export -a components=("tob-db" "tob-api" "tob-web")
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
-export -a builds=("nginx-runtime" "angular-builder" "lib-indy")
+export -a builds=("nginx-runtime" "angular-builder")
 
 # The images to be tagged after build
-export -a images=("angular-on-nginx" "django" "solr" "schema-spy")
+export -a images=("angular-on-nginx" "django" "schema-spy")
 
 # The routes for the project
-export -a routes=("angular-on-nginx" "django" "solr" "schema-spy")
+export -a routes=("angular-on-nginx" "django" "schema-spy")
 
 # Load in any overrides
 if [ -f ./settings.local.sh ]; then
