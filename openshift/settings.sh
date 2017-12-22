6a6ef61ddc6c3561dca36a3744370ae674d5990c
Stephen Curran <swcurran@gmail.com>
2017-12-22 11:27:52 -0800

    Change settings from arrays to lists because bash

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,21 +1,23 @@
+# bash script to be sourced to set environment variables for OpenShift scripts
 export PROJECT_NAMESPACE="jag-csb-jes-family-protection-order"
 export PROJECT_OS_DIR=${PROJECT_OS_DIR:-../../openshift}
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export -a skip_git_overrides=("schema-spy-build.json")
+export skip_git_overrides="schema-spy-build.json"
 export GIT_URI="https://github.com/bcgov/Family-Protection-Order.git"
 export GIT_REF="master"
 
 # The project components
-export -a components=("fpo-db" "fpo-api" "fpo-web")
+# - defaults to the support the Simple Project Structure
+export components=${components:-"fpo-db fpo-api fpo-web"}
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
-export -a builds=("nginx-runtime" "angular-builder")
+export builds=${builds:-"nginx-runtime angular-builder"}
 
 # The images to be tagged after build
-export -a images=("angular-on-nginx" "django" "schema-spy")
+export images=${images:-"angular-on-nginx django schema-spy"}
 
 # The routes for the project
-export -a routes=("angular-on-nginx" "django" "schema-spy")
+export routes=${routes:-"angular-on-nginx django schema-spy"}
