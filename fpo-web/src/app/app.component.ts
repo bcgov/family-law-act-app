09925a663c478e44be2b4d70d683849ad63d588a
Andrew Whitehead <andrew@1crm.com>
2018-03-26 10:27:06 -0700

    scroll to top on page navigation; don't compress sidebar content when hiding

--- fpo-web/src/app/app.component.ts
+++ fpo-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { InsertService } from './insert/insert.service';
@@ -8,16 +9,32 @@ import { InsertService } from './insert/insert.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = '';
   previousUrl: string;
+  isPopState: boolean;
 
   constructor(
       private renderer: Renderer2,
-      private router: Router
-  ) {
+      private router: Router,
+      private locStrat: LocationStrategy
+  ) { }
+
+  ngOnInit(): void {
+  	this.locStrat.onPopState(() => {
+      this.isPopState = true;
+    });
+
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+
+        if (!this.isPopState) {
+          // scroll to page top only when navigating to a new page (not via history state)
+          window.scrollTo(0, 0);
+          this.isPopState = false;
+        }
+        this.isPopState = false;
+
         let prevSlug = this.previousUrl;
         let nextSlug = event.url.slice(1);
         if (!nextSlug) nextSlug = 'home';
