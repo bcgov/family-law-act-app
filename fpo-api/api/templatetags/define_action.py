eb6fef337c7191cb3cdda90b7e29478967e94ac3
Sigoo Yi <sigooyi@Sigoos-iMac.local>
2018-06-08 17:19:25 -0700

    variables added to DJango template

--- fpo-api/api/templatetags/define_action.py
+++ fpo-api/api/templatetags/define_action.py
@@ -10,24 +10,5 @@ def get_at_index(list, index):
   return list[index]
 
 
-@register.filter('break')
-def break_(loop):
-  '''Breaks from a loop.
-
-  The 'break' filter is used within a loop and takes as input a loop variable,
-  e.g. 'forloop' in case of a for loop. For example, to display the items
-  from list ``items`` up to the first item that is equal to ``end``::
-
-      <ul>
-      {% for item in items %}
-          {% if item == 'end' %}
-              {{ forloop|break }}
-          {% endif %}
-          <li>{{ item }}</li>
-      {% endfor %}
-      </ul>
-
-  Breaking from nested loops is also supported by passing the appropriate loop
-  variable, e.g. ``forloop.parentloop|break``.
-  '''
-  raise StopLoopException(loop, False)
+def to_list(*args):
+  return args
--- fpo-web/src/app/survey/primary.component.ts
+++ fpo-web/src/app/survey/primary.component.ts
@@ -16,6 +16,7 @@ export class SurveyPrimaryComponent implements OnInit {
   public surveyPath: string;
   public surveyJson: any;
   public complete: Function;
+  public data: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -40,7 +41,93 @@ export class SurveyPrimaryComponent implements OnInit {
       this.router.navigate(['result', ok]);
     }
     else if(this.cacheName) {
-      this.resultJson = JSON.stringify(data);
+
+        //add additional data starts here
+        data.listOfChildrenArray = [];
+        data.listOfChildrenWithPOArray = [];
+        data.listOfChildrenWithoutPOArray = [];
+        data.listOfChildren = new String();
+        data.listOfChildrenWithPO = new String();
+        data.listOfPeopleWithPO = data.ApplicantName;
+        data.contactTypeString = new String();
+        data.ArrangeMethodString = new String();
+        data.listOfChildrenWithoutPOString = new String();
+        for (let child of data.ListOfChildren){
+            console.log("Child is "+child["ChildName"]);
+            if (child["ChildNeedsProtection"] == "y"){
+
+                data.listOfChildrenWithPOArray.push(child["ChildName"]);
+
+                // data.listOfChildrenWithPO.concat
+            }
+            else {
+                data.listOfChildrenWithoutPOArray.push(child["ChildName"]);
+            }
+            // data.listOfChildren.push(child["ChildName"]);
+        }
+        for (let childWithPO of data.listOfChildrenWithPOArray){
+            console.log("child name before if is " + childWithPO);
+            if(childWithPO == data.listOfChildrenWithPOArray[data.listOfChildrenWithPOArray.length-1]){
+                console.log("last child name is " + childWithPO);
+                data.listOfChildrenWithPO = data.listOfChildrenWithPO + " or "+childWithPO;
+            }
+            else if(childWithPO == data.listOfChildrenWithPOArray[0]){
+                data.listOfChildrenWithPO = data.listOfChildrenWithPO + childWithPO;
+            }
+            else{
+                data.listOfChildrenWithPO = data.listOfChildrenWithPO + ", " + childWithPO;
+            }
+        }
+        for (let childWithoutPO of data.listOfChildrenWithoutPOArray){
+            console.log("child name before if is " + childWithoutPO);
+            if(childWithoutPO == data.listOfChildrenWithoutPOArray[data.listOfChildrenWithoutPOArray.length-1]){
+                console.log("last child name is " + childWithoutPO);
+                data.listOfChildrenWithoutPOString = data.listOfChildrenWithoutPO + " or "+childWithoutPO;
+            }
+            else if(childWithoutPO == data.listOfChildrenWithoutPOArray[0]){
+                data.listOfChildrenWithoutPOString = data.listOfChildrenWithoutPO + childWithoutPO;
+            }
+            else{
+                data.listOfChildrenWithoutPOString = data.listOfChildrenWithoutPO + ", " + childWithoutPO;
+            }
+        }
+        if (data.listOfChildrenWithPO != null){
+            data.listOfPeopleWithPO = data.listOfPeopleWithPO +", " +data.listOfChildrenWithPO;
+        }
+        if (data.listOfChildren != null){
+            data.listOfPeople = data.listOfPeople +", " +data.listOfChildren;
+        }
+
+        console.log("appended child is " + data.listOfChildrenWithPOArray);
+        console.log("appended children in string is " + data.listOfChildrenWithPO);
+        console.log("people who need po are " + data.listOfPeopleWithPO);
+        for (let contactType of data.RespondentApplicantContactType){
+            if (contactType = data.RespondentApplicantContactType[0]){
+                data.contactTypeString = data.contactTypeString + contactType;
+            }
+            else if (contactType = data.RespondentApplicantContactType[data.RespondentApplicantContactType.length-1]){
+                data.contactTypeString = data.contactTypeString + " and " + contactType;
+            }
+            else {
+                data.contactTypeString = data.contactTypeString + ", " + contactType;
+            }
+        }
+        for (let ArrangeMethod of data.RespondentApplicantArrangeMethods){
+            if (ArrangeMethod = data.RespondentApplicantContactType[0]){
+                data.ArrangeMethodString = data.ArrangeMethodString + ArrangeMethod;
+            }
+            else if (ArrangeMethod = data.RespondentApplicantArrangeMethods[data.RespondentApplicantArrangeMethods.length-1]){
+                data.ArrangeMethodString = data.ArrangeMethodString + " and " + ArrangeMethod;
+            }
+            else {
+                data.ArrangeMethodString = data.ArrangeMethodString + ", " + ArrangeMethod;
+            }
+        }
+        //add additional data ends here
+        this.data = data;
+
+        this.resultJson = JSON.stringify(data);
+        console.log("resultJson is" + this.resultJson);
       this.printUrl = this.dataService.getApiUrl('survey-print/' + this.cacheName);
     }
   }
