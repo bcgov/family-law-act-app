a8994a3adbb5e80844f36078f50f80514cf05fd1
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-02-19 09:59:25 -0800

    PFCF-70

--- fpo-web/src/app/home/home.component.ts
+++ fpo-web/src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  typeOfServiceForm: FormGroup;
 
-  constructor() { }
+  constructor(private router:Router, private builder: FormBuilder) {
+    this.constructForm();
+   }
 
-  ngOnInit() {
+  constructForm() {
+    this.typeOfServiceForm = this.builder.group({
+      serviceRadio: new FormControl('', Validators.required)
+    })
   }
+
+  submitServiceTypeForm() {
+    let typeOfService = this.typeOfServiceForm.get('serviceRadio').value;
+    if(typeOfService === "returningUser") {
+      this.router.navigate(['order', typeOfService, 'login']);
+    } else {
+      this.router.navigate(['order', typeOfService]);
+    }
+  }
+
+  get serviceRadio() { return this.typeOfServiceForm.get('serviceRadio'); }
 
 }
