bf3d8edce3e0a532792a2441de30aea26f0fc30a
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2020-02-20 09:57:12 -0800

    Skeleton for downloadable forms -v1

---
+++ fpo-web/src/app/landingPage/caseMgmtLanding/caseMgmt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+
+@Component({
+    selector: 'case',
+    templateUrl: './caseMgmt.component.html',
+    styleUrls: ['./caseMgmt.component.scss']
+})
+export class CaseMgmtComponent {
+}
