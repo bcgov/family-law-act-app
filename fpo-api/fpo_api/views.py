69de7512e6c0097c076ecc29a8054043d03aa0ef
Jamie Popkin <popkinj@littleearth.ca>
2020-03-23 16:30:18 -0700

    Hope this fixes the cookie error

--- fpo-api/fpo_api/views.py
+++ fpo-api/fpo_api/views.py
@@ -5,11 +5,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from api.models.User import User
 
+# Just for testing
+from django.views.decorators.csrf import csrf_exempt
+
 def health(request):
     """
     Health check for OpenShift
     """
     return HttpResponse(User.objects.count())
 
+# Testing
+@csrf_exempt
 def testing(request):
+    """
+    request.method  -> Look for POST
+    request.content_params -> Care about params????
+    request.POST['data'] -> Here is the data
+    """
     return HttpResponse("Hello Testing")
