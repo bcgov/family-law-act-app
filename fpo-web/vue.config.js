0897fd0b1d7b294c371139b49da1eaeccd7edc89
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2020-04-26 02:41:35 -0700

    Fixed the fpo-web build issue

--- fpo-web/vue.config.js
+++ fpo-web/vue.config.js
@@ -10,4 +10,5 @@ module.exports = {
         appendTsSuffixTo: [/\.vue$/],
       });
   },
+  parallel: false, // https://stackoverflow.com/questions/59951379/vue-cli-upgrade-from-v3-to-v4-breaks-build-process-with-thread-loader-error-can
 };
