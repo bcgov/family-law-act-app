de7c4fabf46499578155da5fac162f3eb7bf90e5
Andrew Whitehead <cywolf@gmail.com>
2020-05-25 17:28:51 -0700

    fix pdf output
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- fpo-web/src/components/steps/print/PrintApplication.vue
+++ fpo-web/src/components/steps/print/PrintApplication.vue
@@ -4,13 +4,19 @@
       <div class="row">
         <div class="col-md-12">
           <h1>Print Your Application Forms</h1>
-          <p>Print the forms and file at your local court registry.</p>
+          <p>
+            Print the forms and file at your local court registry.
+          </p>
           <div class="printSection">
             <div class="printAlign">
               <center>
-                <a href="printFPO" v-on:click.prevent="onDownload()" class="btn btn-success btn-lg">
-                  <span class="fa fa-print btn-icon-left"></span> Print Your
-                      Application
+                <a
+                  href="printFPO"
+                  v-on:click.prevent="onDownload()"
+                  class="btn btn-success btn-lg"
+                >
+                  <span class="fa fa-print btn-icon-left"></span>
+                  Print Your Application
                 </a>
               </center>
             </div>
@@ -25,7 +31,7 @@
 import { Step } from "@/models/step";
 import PageBase from "../PageBase.vue";
 import axios from "axios";
-import { Page } from 'survey-vue';
+import { Page } from "survey-vue";
 
 export default {
   name: "print-application",
@@ -41,49 +47,29 @@ export default {
       return this.$store.getters["application/getNavigation"][1].result;
     },
     onDownload: function() {
-      //axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-      //axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'http://localhost:8080';
-      //axios.defaults.headers.post['Access-Control-Allow-Methods'] = 'POST';
       axios
-        .post('http://localhost:8081/form?name=application-about-a-protection-order', {
-        //.post('http://localhost:8083/pdf?filename=fpo.pdf', {
-          //responseType: 'blob',
-          data: this.getFPOResultData(),
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/pdf'
+        .post(
+          "http://localhost:8081/form?name=application-about-a-protection-order",
+          this.getFPOResultData(),
+          {
+            responseType: "blob",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/pdf"
+            }
           }
-        })
+        )
         .then(res => {
-
-          // let binaryString = res.data;
-
-          // let binaryLen = binaryString.length;
-
-          // let bytes = new Uint8Array(binaryLen);
-
-          // for (let i = 0; i < binaryLen; i++) {
-          //   let ascii = binaryString.charCodeAt(i);
-          //   bytes[i] = ascii;
-          // }
-
-          // console.log("bytes array = " + bytes);
-
-          const blob = new Blob([res.data], { type: 'application/pdf' });
-          const link = document.createElement('a');
+          const blob = res.data;
+          const link = document.createElement("a");
           link.href = URL.createObjectURL(blob);
           document.body.appendChild(link);
           link.download = "fpo.pdf";
           link.click();
-          sleep(2000);
-          URL.revokeObjectURL(link.href);
-
-          // open PDf in a new window
-          // const fileURL = URL.createObjectURL(blob);
-          // window.open(fileURL);
+          setTimeout(() => URL.revokeObjectURL(link.href), 1000);
         })
         .catch(err => {
-          console.log("error = " + err.response);
+          console.error(err);
         });
     }
   },
@@ -105,7 +91,7 @@ export default {
 .printSection {
   border: 2px solid rgba($gov-pale-grey, 0.7);
   border-radius: 18px;
-  width: 100%
+  width: 100%;
 }
 .printAlign {
   padding: 20px;
