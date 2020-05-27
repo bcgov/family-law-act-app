15f44ceb179553ccb97c356bcd9c525c27a70ada
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2020-05-26 18:17:11 -0700

    fixing reverted changes

--- fpo-api/api/models/SurveyResult.py
+++ fpo-api/api/models/SurveyResult.py
@@ -2,19 +2,19 @@ from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+
+
 class SurveyResult(models.Model):
-    create_timestamp = models.DateTimeField(
-        auto_now_add=True, blank=True, null=True
-    )
-    update_timestamp = models.DateTimeField(
-        auto_now=True, blank=True, null=True
-    )
-    completed_timestamp = models.DateTimeField(
-        auto_now=True, blank=True, null=True
-    )
+    create_timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_timestamp = models.DateTimeField(auto_now=True, blank=True, null=True)
+    completed_timestamp = models.DateTimeField(auto_now=True, blank=True, null=True)
 
+    collection = models.CharField(max_length=100, default="", blank=True)
     survey_type = models.CharField(max_length=100)
-
     result = JSONField(blank=True)
 
-    user = models.ForeignKey('User', related_name='survey_results')
+    user = models.ForeignKey(
+        "User", related_name="survey_results", on_delete=models.CASCADE
+    )
--- fpo-api/api/urls.py
+++ fpo-api/api/urls.py
@@ -1,15 +1,10 @@
 """
     REST API Documentation for Family Protection Order
-
     OpenAPI spec version: v1
-
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,20 +19,21 @@ from rest_framework.schemas import SchemaGenerator
 from rest_framework.views import APIView
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_swagger import renderers
+
 # generated views
 from . import views
+
 # custom views
 
 from . import survey
 
+
 class SwaggerSchemaView(APIView):
     permission_classes = [AllowAny]
-    renderer_classes = [
-        renderers.OpenAPIRenderer,
-        renderers.SwaggerUIRenderer
-    ]
+    renderer_classes = [renderers.OpenAPIRenderer, renderers.SwaggerUIRenderer]
     _ignore_model_permissions = True
     exclude_from_schema = True
+
     def get(self, request):
         generator = SchemaGenerator()
         schema = generator.get_schema(request=request)
@@ -46,16 +42,22 @@ class SwaggerSchemaView(APIView):
 
 urlpatterns = [
     # Swagger documentation
-    #url(r'^$', SwaggerSchemaView.as_view()),
-
-    url(r'^accept-terms$', views.AcceptTermsView.as_view()),
-
-    url(r'^survey-cache/(?P<name>[a-zA-Z0-9_\-:]+)$', survey.SurveyCacheView.as_view()),
-    url(r'^survey-cache/(?P<name>[a-zA-Z0-9_\-:]+)/(?P<id>[a-zA-Z0-9_\-:]+)$', survey.SurveyCacheView.as_view()),
-
-    url(r'^survey-print/(?P<name>[a-zA-Z0-9_\-:]+)$', views.SurveyPdfView.as_view()),
-
-    url(r'^user-info$', views.UserStatusView.as_view()),
+    # url(r'^$', SwaggerSchemaView.as_view()),
+    url(r"^accept-terms$", views.AcceptTermsView.as_view()),
+    url(
+        r"^survey-result/(?P<collection>[a-zA-Z0-9_\-:]+)/(?P<type>[a-zA-Z0-9_\-:]+)$",
+        survey.SurveyResultView.as_view(),
+    ),
+    url(
+        r"^survey-result/(?P<collection>[a-zA-Z0-9_\-:]+)/(?P<type>[a-zA-Z0-9_\-:]+)/"
+        r"(?P<id>[a-zA-Z0-9_\-:]+)$",
+        survey.SurveyResultView.as_view(),
+    ),
+    url(
+        r"^survey-print/(?P<collection>[a-zA-Z0-9_\-:])/(?P<type>[a-zA-Z0-9_\-:]+)$",
+        views.SurveyPdfView.as_view(),
+    ),
+    url(r"^user-info$", views.UserStatusView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
