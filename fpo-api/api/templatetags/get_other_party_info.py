a6f52a24b691bad795c00832352234eb85aa720f
marzmehr <marzmehr@uvic.ca>
2020-09-23 10:44:59 -0700

    PFCF-202: Fixing API to handle non-existing fields

--- fpo-api/api/templatetags/get_other_party_info.py
+++ fpo-api/api/templatetags/get_other_party_info.py
@@ -6,28 +6,53 @@ register = template.Library()
 def get_other_party_info(selectedPOOrder, protectionWhom, otherParty):
     
     info = [] 
-
-    if selectedPOOrder['orderType'] == 'needPO' :
-        dob = 'unknown'
-        address = ''
-        contactInfo = ''
-        if protectionWhom['RespondentDOBExact'] == 'y':
-            dob = protectionWhom['RespondentDOB']
-        if hasattr(protectionWhom, 'RespondentAddress'):
-            address = protectionWhom['RespondentAddress']
-        if hasattr(protectionWhom,'RespondentContact'):
-            contactInfo = protectionWhom['RespondentContact']
-            
-        info = [{'name':protectionWhom['RespondentName'], 'DOB': dob , 'address': address ,'contact': contactInfo}]
-    
-    elif selectedPOOrder['orderType'] == 'changePO' or selectedPOOrder['orderType'] == 'terminatePO' :    
-        for party in otherParty:
+    if 'orderType' in selectedPOOrder:
+        if selectedPOOrder['orderType'] == 'needPO' :
             dob = 'unknown'
-            if party['knowDob'] == 'y':
-                dob = party['dob']
-            info.append({'name':party['name'], 'DOB': dob , 'address': party['address'] ,'contact': party['contactInfo']})
-   
-    else:
-        info = [{'name':{'first': '','middle': '', 'last': ''}, 'address': '' ,'contact': ''}]
+            name = ''
+            address = ''
+            contactInfo = ''
+
+            if 'RespondentDOBExact' in protectionWhom:
+                if protectionWhom['RespondentDOBExact'] == 'y':
+                    if 'RespondentDOB' in protectionWhom:
+                        dob = protectionWhom['RespondentDOB']
+            
+            if 'RespondentName' in protectionWhom:
+                name = protectionWhom['RespondentName']
+            
+            if 'RespondentAddress' in protectionWhom:
+                address = protectionWhom['RespondentAddress']
+            
+            if 'RespondentContact' in protectionWhom:
+                contactInfo = protectionWhom['RespondentContact']
+                
+            info = [{'name':name, 'DOB': dob , 'address': address ,'contact': contactInfo}]
+        
+        elif selectedPOOrder['orderType'] == 'changePO' or selectedPOOrder['orderType'] == 'terminatePO' :    
+            for party in otherParty:
+                dob = 'unknown'
+                name = ''
+                address = ''
+                contactInfo = ''
+
+                if 'knowDob' in party:
+                    if party['knowDob'] == 'y':
+                        if 'dob' in party:
+                            dob = party['dob']
+
+                if 'name' in party:
+                    name = party['name']
+                
+                if 'address' in party:
+                    address = party['address']
+                
+                if 'contactInfo' in party:
+                    contactInfo = party['contactInfo']
+                
+                info.append({'name':name, 'DOB': dob , 'address': address ,'contact': contactInfo})
+    
+        else:
+            info = [{'name':{'first': '','middle': '', 'last': ''}, 'address': '' ,'contact': ''}]
 
     return info
