c90c689be1fe51da36f7575f20f7c3e4893c69bc
NavpreetGrewal <ngrewal002@gmail.com>
2020-09-28 09:19:20 -0700

    Add force script name

--- fpo-api/fpo_api/settings.py
+++ fpo-api/fpo_api/settings.py
@@ -240,7 +240,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     )
 }
-#FORCE_SCRIPT_NAME = os.getenv("WEB_BASE_HREF", "/family-law-act/") #______________DEVELOP
+FORCE_SCRIPT_NAME = os.getenv("WEB_BASE_HREF", "/family-law-act/")
 LOGOUT_REDIRECT_URL = os.getenv("LOGOUT_REDIRECT_URL", "/family-law-act/")
 # For development (when no SiteMinder available)
 # OVERRIDE_USER_ID = os.getenv("OVERRIDE_USER_ID")
