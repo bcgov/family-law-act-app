16af5ac99d19db82a57e335c5d2a3ec21a1549f5
Travis Semple <travis8814@gmail.com>
2020-12-08 00:27:14 -0800

    Revert "Merge remote-tracking branch 'origin/develop' into PFCF-188"
    This reverts commit 2614c5ce1f2edc6d17c6908443ebfc179ef7212f, reversing
    changes made to ea7487766fc0786fde820edc1c91938763cc7dc7.

--- fpo-web/vue.config.js
+++ fpo-web/vue.config.js
@@ -2,7 +2,7 @@ const webBaseHref = process.env.WEB_BASE_HREF || '/';
 module.exports = {
   publicPath: webBaseHref,
   devServer: {
-    proxy: {  
+    proxy: {
       "/api": {
         target: "http://localhost:8081",
         pathRewrite: { '^/apply-for-family-order': '' },
@@ -27,5 +27,5 @@ module.exports = {
         appendTsSuffixTo: [/\.vue$/]
       });
   },
-  parallel: false // https://stackoverflow.com/questions/59951379/vue-cli-upgrade-from-v3-to-v4-breaks-build-process-with-thread-loader-error-can 
+  parallel: false // https://stackoverflow.com/questions/59951379/vue-cli-upgrade-from-v3-to-v4-breaks-build-process-with-thread-loader-error-can
 };
