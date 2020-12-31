7d4852cae916e6de88d460eca75ee641fa005fb4
Wade Barnes <wade.barnes@shaw.ca>
2020-12-31 13:48:17 -0800

    Add backup configuration
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -5,4 +5,4 @@ export GIT_REF="master"
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
-export skip_git_overrides="schema-spy-build.yaml"
+export skip_git_overrides="schema-spy-build.yaml backup-build.yaml"
