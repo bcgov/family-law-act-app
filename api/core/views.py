ccf8b943c8cd2bc5c699f07e6bfc6efb004e558e
Travis Semple <travis8814@gmail.com>
2021-02-03 15:00:28 -0800

    Small update for docker-compose.yml and manage.

--- api/core/views.py
+++ api/core/views.py
@@ -13,8 +13,9 @@ def health(request):
 
 
 def login(request):
-
-    """ Check if we are already logged in. If we are don't redirect to SSO. """
+    """
+    Check if we are already logged in. If we are don't redirect to SSO.
+    """
     logged_in = isinstance(request.user, User)
     if logged_in:
         return HttpResponseRedirect(settings.FORCE_SCRIPT_NAME)
