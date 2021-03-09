406652875b82d41718e8209766ee3e1ff663fa5a
Travis Semple <travis8814@gmail.com>
2021-03-09 09:35:02 -0800

    Remove 180 degree rotation on 0 degrees.

--- api/core/pdf.py
+++ api/core/pdf.py
@@ -43,8 +43,6 @@ def rotate_image(file, rotation):
         image_rotated = image.transpose(Image.ROTATE_270)
     elif rotation == 270:
         image_rotated = image.transpose(Image.ROTATE_90)
-    else:
-        image_rotated = image.transpose(Image.ROTATE_180)
     image_bytearray = io.BytesIO()
     image_rotated.save(image_bytearray, format=image.format)
     return image_bytearray.getvalue()
