fa5cf41d09aa393ccc899d9b2de5c274d6e299e4
marzmehr <marzmehr@uvic.ca>
2021-04-14 10:15:28 -0700

    PFCF-55: adding the 'child-support-agreement-order' and 'about existing child support order agreement' pages

--- web/src/components/steps/familyLawMatter/childSupport/ChildSupportOrderAgreement.vue
+++ web/src/components/steps/familyLawMatter/childSupport/ChildSupportOrderAgreement.vue
@@ -30,10 +30,7 @@ export default class ChildSupportOrderAgreement extends Vue {
     step!: stepInfoType;
 
     @applicationState.State
-    public steps!: any
-
-    @applicationState.State
-    public applicantName!: nameInfoType;
+    public steps!: any    
 
     @applicationState.Action
     public UpdateGotoPrevStepPage!: () => void
@@ -44,10 +41,7 @@ export default class ChildSupportOrderAgreement extends Vue {
     @applicationState.Action
     public UpdateStepResultData!: (newStepResultData: stepResultInfoType) => void
 
-    survey = new SurveyVue.Model(surveyJson);
-    surveyJsonCopy;
-    childData = [];
-    overAgeChildren = [];
+    survey = new SurveyVue.Model(surveyJson);    
     currentStep=0;
     currentPage=0;
    
@@ -68,92 +62,26 @@ export default class ChildSupportOrderAgreement extends Vue {
         this.reloadPageInformation();
     }
 
-    public initializeSurvey(){
-        this.adjustSurveyForChildren();
-        this.adjustSurveyForOtherParties();
-        this.survey = new SurveyVue.Model(this.surveyJsonCopy);
+    public initializeSurvey(){       
+        this.survey = new SurveyVue.Model(surveyJson);
         this.survey.commentPrefix = "Comment";
         this.survey.showQuestionNumbers = "off";
         this.survey.showNavigationButtons = false;
         surveyEnv.setGlossaryMarkdown(this.survey);
-    }
-
-    public adjustSurveyForChildren(){
-
-        this.surveyJsonCopy = JSON.parse(JSON.stringify(surveyJson));                
-        this.surveyJsonCopy.pages[0].elements[2].elements[8]["choices"]=[];
-        this.childData = [];
-        this.overAgeChildren = [];        
-
-        if (this.step.result && this.step.result['childData']) {
-            const childData = this.step.result['childData'];            
-            for (const child of childData){
-                this.childData.push(child);
-                if (this.getAge(child.dob) >= 19){
-                    this.overAgeChildren.push(Vue.filter('getFullName')(child.name))
-                }
-                this.surveyJsonCopy.pages[0].elements[2].elements[8]["choices"].push(Vue.filter('getFullName')(child.name));
-            }
-        }
-    }
-
-    public adjustSurveyForOtherParties(){        
-             
-        this.surveyJsonCopy.pages[0].elements[2].elements[7]["choices"]=[Vue.filter('getFullName')(this.applicantName)];
-
-        if (this.steps[2].result && this.steps[2].result['otherPartyCommonSurvey'] && this.steps[2].result['otherPartyCommonSurvey'].data) {
-            const otherPartyData = this.steps[2].result['otherPartyCommonSurvey'].data;            
-            for (const otherParty of otherPartyData){
-               this.surveyJsonCopy.pages[0].elements[2].elements[7]["choices"].push(Vue.filter('getFullName')(otherParty.name));
-            }
-        }
-    }
+    }   
     
     public addSurveyListener(){
         this.survey.onValueChanged.add((sender, options) => {
-            console.log(this.childData);
-            console.log(this.overAgeChildren);
             console.log(options)
-            if (options.name == "childrenRequireSupportChoices"){
-                const overAgeSelected = [];
-                for (const overAge of this.overAgeChildren) {
-                    if (options.value.includes(overAge)){
-                        overAgeSelected.push(overAge);
-                    }
-                }
-                if (overAgeSelected.length > 0) {
-                    this.survey.setVariable("overAgeChildSelected", true);
-                } else {
-                    this.survey.setVariable("overAgeChildSelected", false);
-                }
-                
-            } 
 
         })
     }
     
     public reloadPageInformation() {        
-        if (this.step.result && this.step.result['childSupportSurvey']) {
-            this.survey.data = this.step.result['childSupportSurvey'].data;
-
-            const overAgeSelected = [];
-            for (const overAge of this.overAgeChildren) {
-                if (this.survey.data["childrenRequireSupportChoices"] && 
-                    this.survey.data["childrenRequireSupportChoices"].includes(overAge)){
-                    overAgeSelected.push(overAge);
-                }
-            }
-            if (overAgeSelected.length > 0) {
-                this.survey.setVariable("overAgeChildSelected", true);
-            } else {
-                this.survey.setVariable("overAgeChildSelected", false);
-            }
-
+        if (this.step.result && this.step.result['childSupportOrderAgreementSurvey']) {
+            this.survey.data = this.step.result['childSupportOrderAgreementSurvey'].data; 
             Vue.filter('scrollToLocation')(this.$store.state.Application.scrollToLocationName);            
         }
-
-        this.survey.setVariable("ApplicantName", Vue.filter('getFullName')(this.applicantName));
-
 
         this.currentStep = this.$store.state.Application.currentStep;
         this.currentPage = this.$store.state.Application.steps[this.currentStep].currentPage;
@@ -168,32 +96,11 @@ export default class ChildSupportOrderAgreement extends Vue {
         if(!this.survey.isCurrentPageHasErrors) {
             this.UpdateGotoNextStepPage()
         }
-    }  
-
-    public getAge(dateOfBirth: string){
-        const dob = dateOfBirth.split('-')
-        
-        const today_date = new Date();
-        const today_year = today_date.getFullYear();
-        const today_month = today_date.getMonth();
-        const today_day = today_date.getDate();
-        let age = today_year - Number(dob[0]);
-
-        if ( today_month < (Number(dob[1]) - 1)) {
-            age--;
-        }
-        if (((Number(dob[1]) - 1) == today_month) && (today_day < Number(dob[2]))) {
-            age--;
-        }
-
-        return age;
     }
     
     beforeDestroy() {
-        Vue.filter('setSurveyProgress')(this.survey, this.currentStep, this.currentPage, 50, true);
-        
-        this.UpdateStepResultData({step:this.step, data: {childSupportSurvey: Vue.filter('getSurveyResults')(this.survey, this.currentStep, this.currentPage)}})
-
+        Vue.filter('setSurveyProgress')(this.survey, this.currentStep, this.currentPage, 50, true);        
+        this.UpdateStepResultData({step:this.step, data: {childSupportOrderAgreementSurvey: Vue.filter('getSurveyResults')(this.survey, this.currentStep, this.currentPage)}})
     }
 }
 </script>
