be522997cf9c00696b7db740d4e004e834aeac17
Travis Semple <travis8814@gmail.com>
2021-06-15 19:41:16 -0700

    Update types version number.

--- web/src/types/Application/index.ts
+++ web/src/types/Application/index.ts
@@ -12,7 +12,7 @@ import { aboutPOSurveyInfoType, allchildrenInfoType, backgroundSurveyInfoType, E
 //This is what our database saves.
 export interface applicationStepOnlyInfoType {
   steps: stepInfoType[];
-  types_version?: "1.0";
+  types_version?: "1.1";
 }
 
 export interface applicationInfoType extends applicationStepOnlyInfoType {   
