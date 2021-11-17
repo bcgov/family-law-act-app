f0da7d7c05440845ce69a8db901a9cdf4957c30f
marzmehr <marzmehr@uvic.ca>
2021-11-17 14:48:42 -0800

    PFCF-342:  Adding instructions for the remaining pathways

--- web/src/components/steps/caseMgmt/reviewCM/RequiredForm.ts
+++ web/src/components/steps/caseMgmt/reviewCM/RequiredForm.ts
@@ -1,15 +1,20 @@
 import store from '@/store';
 
-export function whichCaseMgmtForm() {
-
-    const stPgNo = store.state.Application.stPgNo;
-
+export function whichCaseMgmtForm(stepCMresult?) {
+    
     const requiredForm = [];
-    const stepCM = store.state.Application.steps[stPgNo.CM._StepNo]   
+    
+    let resultCm = stepCMresult
+    
+    if(!stepCMresult){
+        const stPgNo = store.state.Application.stPgNo;
+        const stepCM = store.state.Application.steps[stPgNo.CM._StepNo]   
+        resultCm = stepCM.result;
+    }
 
-    if(stepCM.result?.withoutNoticeOrAttendanceSurvey?.data && stepCM.result?.cmQuestionnaireSurvey?.data){
-        const withoutNoticeOrAttendanceData = stepCM.result.withoutNoticeOrAttendanceSurvey.data;
-        const determinedItemType = determineNoticeTurquoise(stepCM.result)
+    if(resultCm?.withoutNoticeOrAttendanceSurvey?.data && resultCm?.cmQuestionnaireSurvey?.data){
+        const withoutNoticeOrAttendanceData = resultCm.withoutNoticeOrAttendanceSurvey.data;
+        const determinedItemType = determineNoticeTurquoise(resultCm)
         if(withoutNoticeOrAttendanceData?.needWithoutNotice == 'y' && determinedItemType.needNotice && determinedItemType.nonTurquoise){
             requiredForm.push('P10','P11');                
         }
