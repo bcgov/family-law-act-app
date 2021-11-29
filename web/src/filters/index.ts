b15a16e64575256c892406bee1d3bddd529ec29b
marzmehr <marzmehr@uvic.ca>
2021-11-29 14:07:32 -0800

    PFCF-348: Resolve the issue with inconsistency between location names received through locations API in dev/test/prod

--- web/src/filters/index.ts
+++ web/src/filters/index.ts
@@ -613,11 +613,17 @@ Vue.filter('surveyChanged', function(type: string) {
 
 Vue.filter('includedInRegistries', function(locationName: string, registryType: string) {
 
-	if (registryType == 'parenting-education' && ParentingEducationRegistries.includes(locationName)){
+	
+	const locationsInfo = store.state.Common.locationsInfo;
+	const location = locationsInfo.filter(locationInfo => locationInfo.name == locationName)[0];
+	
+	if (!location) return false;
+
+	if (registryType == 'parenting-education' && ParentingEducationRegistries.includes(location.id)){
 		return true;
-	} else if (registryType == 'early-resolutions' && EarlyResolutionsRegistries.includes(locationName)){
+	} else if (registryType == 'early-resolutions' && EarlyResolutionsRegistries.includes(location.id)){
 		return true;
-	} else if (registryType == 'family-justice' && FamilyJusticeRegistries.includes(locationName)){
+	} else if (registryType == 'family-justice' && FamilyJusticeRegistries.includes(location.id)){
 		return true;
 	} else {
 		return false;
