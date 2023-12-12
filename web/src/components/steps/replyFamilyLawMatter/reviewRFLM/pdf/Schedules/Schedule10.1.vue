fb9ac5d5ab156528a4faeee3d37ca9fbe2d84cb6
marzmehr <mars.mehr@gmail.com>
2023-12-11 18:49:26 -0800

    PFCF-443: Applying changes to form 6 based on leg changes

---
+++ web/src/components/steps/replyFamilyLawMatter/reviewRFLM/pdf/Schedules/Schedule10.1.vue
@@ -0,0 +1,142 @@
+<template>
+    <div v-if="dataReady">
+
+<!-- <Page 7> --> 
+<!-- <Header> -->
+        <div>
+             <div class="new-page" />
+            <div style="text-align:center;"><b>
+                SCHEDULE 10.1 – REPLY TO AN APPLICATION ABOUT PROPERTY DIVISION IN RESPECT OF A COMPANION ANIMAL – NEW
+            </b></div>
+            <div style="text-align:center;"><b>This is Schedule 10.1 to the Reply to an Application About a Family Law Matter</b></div>
+
+            <div style="margin:1rem 0; text-align:justify">
+                <i>
+                    This schedule must be completed only if you are disagreeing with a new application by 
+                    the other party for a property division order in respect of a companion animal. The order 
+                    they applied for about property division in respect of a companion animal can be found in
+                    Schedule 11 of their Application About a Family Law Matter.
+                </i>
+            </div>
+
+<!-- <1> -->
+            <section class="resetquestion">
+                <div style="margin:0 0 0 1.5rem;">
+                    Select only one of the options below                   
+                </div>
+                <div style="margin:0 0 2rem 3.25rem;">
+                    <check-box  :check="exCompInfo.setAside?'yes':''" text="I am (or was) the other party’s spouse"/>
+                    <check-box  :check="exCompInfo.replace?'yes':''" text="I have never been the other party’s spouse"/>
+                    <div>Please describe your relationship to the other party:</div>                    
+                    <div v-if="exCompInfo.reason" 
+                        class="answerbox">{{exCompInfo.reason}}</div>
+                    <div v-else style="margin-bottom:3rem;"></div>
+                </div>
+            </section>
+
+            <div class="print-block">
+<!-- <2> -->
+                <section>
+                    <div style="margin:1rem 3rem 2rem 1rem;">                      
+                        <div>
+                            I do not agree with the order requested by the other party about 
+                            property division in respect of a companion animal because:
+                        </div>
+                        <div v-if="exCompInfo.replace && exCompInfo.agreementReplacementDetails" 
+                            class="answerbox">{{exCompInfo.agreementReplacementDetails}}</div>
+                        <div v-else style="margin-bottom:3rem;"></div>
+                    </div>
+                </section> 
+            </div>
+
+            <div class="print-block">
+<!-- <3> -->
+                <section>
+                    <div style="display:inline; margin-left:0.35rem">
+                        I believe the court should make the following order for property division in respect of a companion animal:
+                    </div>
+                    <div style="margin:1rem 0 0 1rem;">
+                        <i> 
+                            Note: Under section 97 of the Family Law Act, the court may only make 
+                            an order for ownership and possession of a companion animal <b>by one spouse.</b>
+                        </i>                        
+                    </div>                   
+                    
+                    <div style="margin:1rem 3rem 2rem 1rem;">
+                        <div v-if="exCompInfo.replace && exCompInfo.agreementReplacementDetails" 
+                            class="answerbox">{{exCompInfo.agreementReplacementDetails}}</div>
+                        <div v-else style="margin-bottom:3rem;"></div>
+                    </div>
+                </section> 
+            </div>
+
+        </div>           
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import UnderlineForm from "@/components/utils/PopulateForms/components/UnderlineForm.vue";
+import CheckBox from "@/components/utils/PopulateForms/components/CheckBox.vue";
+import { schedule12DataInfoType } from '@/types/Application/FamilyLawMatter/Pdf';
+import { companionAnimalExistingAgreementDataInfoType } from '@/types/Application/FamilyLawMatter/CompanionAnimal';
+
+@Component({
+    components:{
+        UnderlineForm,
+        CheckBox
+    }
+})
+
+export default class Schedule101 extends Vue {
+
+    @Prop({required:true})
+    result!: any; 
+   
+    dataReady = false;
+    exCompInfo = {} as schedule12DataInfoType;
+   
+    mounted(){
+        this.dataReady = false;      
+        this.extractInfo();       
+        this.dataReady = true;        
+    }    
+
+    public extractInfo(){ 
+        this.exCompInfo = this.getExCompanionAnimalInfo(); 
+    }
+
+    public getExCompanionAnimalInfo(){
+       
+        let exCompInfo = {} as schedule12DataInfoType;
+
+        exCompInfo = {
+            agreementDate: '',
+            reason: '',
+            replace: false,
+            agreementReplacementDetails: '',
+            setAside: false
+        }
+
+        if (this.result.companionAnimalExistingAgreementSurvey){
+            const exCompData: companionAnimalExistingAgreementDataInfoType = this.result.companionAnimalExistingAgreementSurvey;
+            
+            exCompInfo.agreementDate = Vue.filter('beautify-date')(exCompData.agreementDate);
+            exCompInfo.reason = exCompData.setAsideReason;
+            exCompInfo.replace = exCompData.existingAgreementDecision == 'Replaced';
+            exCompInfo.setAside = exCompData.existingAgreementDecision == 'SetAside';
+            if (exCompInfo.replace){
+                exCompInfo.agreementReplacementDetails = exCompData.agreementReplacement?exCompData.agreementReplacement:'';
+            }
+        }
+
+       return exCompInfo;
+   } 
+
+}
+</script>
+
+<style scoped lang="scss" src="@/styles/_pdf.scss">
+
+</style> 
