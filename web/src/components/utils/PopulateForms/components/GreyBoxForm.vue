55d12f80add0bf02fce931cef3e19ffea20ac266
shruthi-rapala <shruthi.rapala@quartech.com>
2024-08-06 11:42:02 -0700

    PFCF - 471 : Form 49 : Completed Affidavit of Personal Service of Protection Order - Adjusted hint text color in grey box

--- web/src/components/utils/PopulateForms/components/GreyBoxForm.vue
+++ web/src/components/utils/PopulateForms/components/GreyBoxForm.vue
@@ -2,11 +2,14 @@
     <div style="display:inline-block;">
         <div v-if="beforetext" style="display:inline-block; margin-right:.25rem;" v-html="beforetext"> {{beforetext}} </div>
         <div style="display:inline-block">
-            <div :style="{position: 'absolute', marginTop:marginTop, fontSize: entryFontSize, background: '#d6d6d6', color:'#000000', minWidth: textwidth, textAlign: 'center'}">{{text ? text : '&nbsp;'}}</div> 
-            <div :style="{borderBottom: '0.7px solid #d6d6d6', minWidth: textwidth, background: '#d6d6d6'}"></div>            
-            <div :style="{position: 'absolute', marginTop:'-1px', marginLeft: hintindent, fontSize: hintFontSize, color:'#d6d6d6'}">
-               <i v-if="italicHint"> {{hint}} </i>
-               <span v-else > {{hint}} </span> 
+            <div
+                :style="{ position: 'absolute', marginTop: marginTop, fontSize: entryFontSize, background: '#d6d6d6', color: '#000000', minWidth: textwidth, textAlign: 'center' }">
+                {{ text ? text : '&nbsp;' }}</div>
+            <div :style="{ borderBottom: '0.7px solid #d6d6d6', minWidth: textwidth, background: '#d6d6d6' }"></div>
+            <div
+                :style="{ position: 'absolute', marginTop: '-1px', marginLeft: hintindent, fontSize: hintFontSize, color: '#8a8a8a' }">
+                <i v-if="italicHint"> {{ hint }} </i>
+                <span v-else> {{ hint }} </span>
             </div>
         </div>
         <div v-if="aftertext" style="display:inline-block; margin-left:.1rem;" v-html="aftertext"> {{aftertext}} </div>
@@ -14,38 +17,40 @@
 </template>
 
 <script lang="ts">
+import { property } from 'underscore';
 import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
 export default class UnderlineForm extends Vue {
-    @Prop({required: true})
+    @Prop({ required: true })
     beforetext!: string;
 
-    @Prop({required: false, default:''})
+    @Prop({ required: false, default: '' })
     aftertext!: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     text!: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     hint!: string;
 
-    @Prop({required: false, default:'100px'})
+    @Prop({ required: false, default: '100px' })
     hintindent!: string;
 
-    @Prop({required: true})
+    @Prop({ required: true })
     textwidth!: string;
 
-    @Prop({required: false, default:'6pt'})
+    @Prop({ required: false, default: '6pt' })
     hintFontSize!: string;
-    
-    @Prop({required: false, default:'11pt'})
+
+    @Prop({ required: false, default: '11pt' })
     entryFontSize!: string;
 
-    @Prop({required:false, default:false})
+    @Prop({ required: false, default: false })
     italicHint!: boolean;
 
-    @Prop({default: '-20px'})
+    @Prop({ default: '-20px' })
     marginTop!: string;
+ 
 }
 </script>
