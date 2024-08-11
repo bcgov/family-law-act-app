badb9294e7d5ef7b454a373408e2b81d07aefed6
QSL\SumathiT <sumathi.thirumani@quartech.com>
2024-08-11 08:43:11 -0700

    Changes to show manul filing of Form 6.

---
+++ web/src/components/steps/familyLawManual/FamilyLawManualStep.vue
@@ -0,0 +1,36 @@
+<template>
+    <step-base v-bind:step="step">
+        <family-law-manual v-bind:step="step" v-if="step.currentPage == stPgNo.RTFLM.FamilyLawManual"/> 
+    </step-base>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop} from 'vue-property-decorator';
+
+import { namespace } from "vuex-class";   
+import "@/store/modules/application";
+const applicationState = namespace("Application");
+
+import StepBase from "../StepBase.vue";
+import FamilyLawManual from "./FamilyLawManual.vue";
+
+import {stepsAndPagesNumberInfoType} from "@/types/Application/StepsAndPages";
+import { stepInfoType } from "@/types/Application";
+
+@Component({
+    components:{
+        StepBase,  
+        FamilyLawManual      
+    }
+})
+export default class FamilyLawManualStep extends Vue {
+
+    @Prop({required: true})
+    step!: stepInfoType;
+
+    @applicationState.State
+    public stPgNo!: stepsAndPagesNumberInfoType;
+
+}
+
+</script>
