38d635ddabbaa20c72576f8501db3021adba9aa9
Taylor, Jonathan <jonathan.taylor@quartech.com>
2024-08-26 16:18:27 -0700

    PFCF-480: Update issues based on testing

--- web/src/components/steps/agreementEnfrc/reviewAE/pdf/Form27Layout.vue
+++ web/src/components/steps/agreementEnfrc/reviewAE/pdf/Form27Layout.vue
@@ -3,13 +3,13 @@
 
 <!-- <Page 1> -->
 <!-- <HEADER> -->
-    <FormHeader :headerTableData="headerTableData" formName="Request to File a Determination of Parenting Coordinator" formNumber="Form 27" formRuleNumer="Rule 133"></FormHeader>
+    <FormHeader :headerTableData="headerTableData" formName="Request to File a Determination of Parenting Coordinator" formNumber="Form 27" formRuleNumber="Rule 133"></FormHeader>
         
     <div style="display: flex; flex-direction: row;">
             <div style="width: 80%; padding-right: 4px;">
                 <div>
                     This Request to File a Determination of Parenting Coordinator provides information about a
-                    determination of a parenting coordinator that a person is filing under the Family Law Act in
+                    determination of a parenting coordinator that a person is filing under the <i>Family Law Act</i> in
                     the Provincial Court. A determination of a parenting coordinator that is filed in Provincial
                     Court is enforceable under the Act as if it were an order of the Provincial Court. 
                 </div>
@@ -72,18 +72,33 @@
                     hintindent="100px"
                     :italicHint="false" :text="firstOtherParty.dob | beautify-date"/>
                 <div style="margin-top: 0.8rem;"></div>
-                <check-box inline="inline" :shiftmark="0" boxMargin="0" shift="10" style="display:inline;margin-left:0rem;" :check="additionalOtherParties.length>0?'yes':''" text="There is an additional party"/>
+                <check-box inline="inline" :shiftmark="0" boxMargin="0" shift="10" style="display:inline;margin-left:0rem;" :check="additionalOtherParties.length>0?'yes':''" text="There is an additional party."/>
+                <br>
                 <div v-for="(otherParty,inx) in additionalOtherParties" :key="inx" :style="inx==0?'display:inline;':'text-indent:-5px;margin-top:1rem;'">
-                    <div style="display: inline-block; font-size: 11pt;">
-                        The <b>additional party's full name</b> is:
-                    </div>
-                    <grey-box-form style="text-indent:4px;display:inline-block; font-size: 9pt;" textwidth="290px"
-                        beforetext="" hint="Full name of party" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="otherParty.name | getFullName" />
-                    <div style="display: inline-block; font-size: 11pt;">
-                        Their <b>date of birth</b> (dd/mmm/yyyy) is:
-                    </div>
-                    <grey-box-form style="text-indent:4px;display:inline-block; font-size: 9pt;" textwidth="290px"
-                        beforetext="" hint="Full name of party" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="otherParty.dob | beautify-date" />
+                    <table>
+                        <tr>
+                            <td style="text-align: right; padding: 6px; width: 330px;">
+                                <div style="display: inline-block; font-size: 11pt; align-content: right;">
+                                    The <b>additional party's full name</b> is:
+                                </div>
+                            </td>
+                            <td style="padding: 6px; width: 230px;">
+                                <grey-box-form style="text-indent:4px;display:inline; font-size: 9pt;" textwidth="210px"
+                                    beforetext="" hint="" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="otherParty.name | getFullName" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: right; padding: 6px; width: 330px;">
+                                <div style="display: inline-block; font-size: 11pt; align-content: right;">
+                                    Their <b>date of birth</b> (dd/mmm/yyyy) is:
+                                </div>
+                            </td>
+                            <td style="padding: 6px; width: 230px;">
+                                <grey-box-form style="text-indent:4px;display:inline; font-size: 9pt;" textwidth="210px"
+                                    beforetext="" hint="" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="otherParty.dob | beautify-date" />
+                            </td>
+                        </tr>
+                    </table>    
                 </div>
             </div>
             <div style="width: 20%;margin-top:-42px;">
@@ -130,17 +145,10 @@
                 <b style="padding-right:3px">4.</b>
                 <div style="display:inline; margin:0 0 0 0.25rem; line-height: 30px;">The parenting coordinator made the determination under:<br/>
                     <i>Select only one of the options below and complete the required information</i><br/></div>
-                <grey-box-form 
-                    style="text-indent:2px;display:inline-block; font-size: 9pt; line-height: 30px;" 
-                    textwidth="55%" 
-                    beforetext="" 
-                    hint="Full name of party"
-                    hintindent="23%"
-                    :italicHint="false" :text="firstOtherParty.name | getFullName"/>
                 <div style="margin-top: 0.8rem;"></div>
                 <check-box inline="inline" :shiftmark="0" boxMargin="0" shift="10" style="display:inline;margin-left:0rem;" :check="form27Info.type == 'writtenAgreement'?'yes':''" text="An <b>agreement</b> made on "/>
                 <grey-box-form style="text-indent:4px;display:inline-block;" textwidth="290px"
-                    beforetext="" hint="(dd/mmm/yyyy)" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="form27Info.agreementDate" />
+                    beforetext="" hint="Date (dd/mmm/yyyy)" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="form27Info.agreementDate" />
                 <div style="margin-top: 0.8rem;"></div>
                 <grey-box-form style="text-indent:4px;display:inline-block;" textwidth="290px"
                     beforetext="<b>between</b>" hint="(Parties to the agreement)" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="form27Info.partiesToAgreement" />
@@ -148,7 +156,7 @@
                 <br/>
                 <check-box inline="inline" :shiftmark="0" boxMargin="0" shift="10" style="display:inline;margin-left:0rem;" :check="form27Info.type == 'courtOrder'?'yes':''" text="A <b>court order</b> made on "/>
                 <grey-box-form style="text-indent:4px;display:inline-block;" textwidth="290px"
-                    beforetext="" hint="(dd/mmm/yyyy)" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="form27Info.orderDate" />
+                    beforetext="" hint="Date (dd/mmm/yyyy)" :italicHint="false" textBackgroundColor="#dedede" hintMargin="152px" :text="form27Info.orderDate" />
             </div>
             <div style="width: 20%;margin-top:-42px;">
                 <NoteBox>
@@ -187,25 +195,45 @@
         <div style="display: flex; flex-direction: row;">
             <div style="width: 80%; padding-right: 4px;">
                 <b style="padding-right:3px">5.</b>
-                <div style="display:inline; margin:0 0 0 0.25rem; line-height: 30px;">My address for service of court documents and 
-                    contact information is: <i>You must provide an address for service and contact number, but it does not have to 
+                <div style="display:inline; margin:0 0 0 0.25rem; line-height: 15px;">My <b>address for service</b> of court documents and 
+                    contact information is:<br/> <i>You must provide an address for service and contact number, but it does not have to 
                     be your own if you don’t want to</i><br/></div>
-                <table class="fullsize" style="margin-top:0 !important;  ">
-                    <tr style="border:1px solid #414142">          
-                        <td colspan="3">Address: <div class="answer">{{yourInfo.address.street}} </div> </td>
+                <table class="compactfullsize" style="margin-top:0.5 !important; margin-left: 2px; font-size: 9pt; background-color: #dedede;">
+                    <tr style="border:2px solid #fff">
+                        <td colspan="3" style="border: 2px solid #fff; padding: 4px;">
+                            Address: 
+                            <div class="answer" style="background-color: #d6d6d6;">
+                                {{ yourInfo.address.street }} 
+                            </div>
+                        </td>
                     </tr>
-                    <tr style="border:1px solid #313132">
-                        <td  >City: <div class="answer">{{yourInfo.address.city}}</div> </td>
-                    <td style="padding-left:50px">Province: <div class="answer">{{yourInfo.address.state}}</div> </td>
-                        <td>Postal Code: <div class="answer">{{yourInfo.address.postcode}}</div> </td>
+                    <tr style="border:2px solid #fff">
+                        <td style="border:2px solid #fff; padding: 4px;">City: <div class="answer" style="background-color: #d6d6d6;">{{ yourInfo.address.city }}</div>
+                        </td>
+                        <td style="padding-left:50px; border:2px solid #fff; padding: 4px;">Province: <div class="answer" style="background-color: #d6d6d6;">{{ yourInfo.address.state }}</div>
+                        </td>
+                        <td style="border:2px solid #fff; padding: 4px;">Postal Code: <div class="answer" style="background-color: #d6d6d6;">{{ yourInfo.address.postcode }}</div>
+                        </td>
                     </tr>
-                    <tr style="border:1px solid #313132">
-                        <td colspan="2">Email: <div class="answer">{{yourInfo.contact.email}}</div> </td>
-                        <td>Telephone: <div class="answer">{{yourInfo.contact.phone}}</div> </td>
+                </table>
+
+                <table class="compactfullsize" style="margin-top:0.5 !important; margin-left: 2px; font-size: 9pt; background-color: #dedede;">
+                    <tr style="border:2px solid #fff">
+                        <td style="border:2px solid #fff; padding: 4px;">Email: <div class="answer" style="background-color: #d6d6d6;">{{ yourInfo.contact.email }}</div>
+                        </td>
+                        <td style="border:2px solid #fff; padding: 4px;">Telephone: <div class="answer" style="background-color: #d6d6d6;">{{ yourInfo.contact.phone }}</div>
+                        </td>
                     </tr>
-                    <tr style="border:1px solid #414142" >
-                        <td v-if="yourInfo.lawyer" colspan="3">Lawyer (if applicable): <div class="answer"> {{yourInfo.lawyerName | getFullName}}</div></td>
-                        <td v-else  colspan="3">Lawyer’s name and firm name (if applicable): </td>
+                </table>
+
+                <table class="compactfullsize" style="margin-top:0.5 !important; margin-left: 2px; font-size: 9pt; background-color: #dedede;">
+                    <tr style="border:1px solid #fff;">
+                        <td v-if="yourInfo.lawyer" colspan="3" style="border:2px solid #fff; padding: 4px;">Lawyer’s name and firm name (if applicable): 
+                            <div class="answer" style="background-color: #d6d6d6;">
+                                {{ yourInfo.lawyerName | getFullName}}
+                            </div>
+                        </td>
+                        <td v-else colspan="3" style="border:2px solid #fff; padding: 4px;" >Lawyer’s name and firm name (if applicable): </td>
                     </tr>
                 </table>
             </div>
